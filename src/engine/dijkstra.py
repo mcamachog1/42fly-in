ce35bb449675366b50698ecfaaae533bc0ad2929
Maryvi Camacho <macamach@c1r7s4.42porto.com>
2026-06-09 12:36:49 +0100

    changes oop

--- src/engine/dijkstra.py
+++ src/engine/dijkstra.py
@@ -5,23 +5,62 @@ from heapq import heappush, heappop
 from src.model.model import Map, ZoneType
 
 
-def adjacent_zones(
-    network: Map,
-    zone: str,
-    exclude: Optional[list[str]] = None
-) -> list[str]:
+class MinCost():
+    def __init__(self, network: Map, start: str) -> None:
+        self.network = network
+        self.start = start
 
-    zones: list[str] = []
-    for conn in network.connections:
-        z1, z2 = conn.name.split('-')
-        if exclude is not None:
-            if z1 in exclude or z2 in exclude:
+
+    def _adjacent_zones(
+        self,
+        zone: str,
+        exclude: Optional[list[str]] = None
+    ) -> list[str]:
+
+        zones: list[str] = []
+        for conn in self.network.connections:
+            z1, z2 = conn.name.split('-')
+            if exclude is not None:
+                if z1 in exclude or z2 in exclude:
+                    continue
+            if z1 == zone:
+                zones.append(z2)
+            elif z2 == zone:
+                zones.append(z1)
+        return zones
+
+
+    def lower_cost_path(self, exclude: Optional[list[str]] = None) -> dict[str, tuple[int, list[str]]]:
+
+        get_hub_object = {hub.name: hub for hub in network.hubs}
+        info: tuple[int, str, list[str]] = (0, self.start, [self.start])
+        lower_cost: dict[str, tuple[int, list[str]]] = {self.start: (0, [self.start])}
+        # list of tuples (cost, zone, path-until-this-zone)
+        heap: list[tuple[int, str, list[str]]] = []
+        heappush(heap, info)
+
+        while len(heap) > 0:
+            cost, zone, path = heappop(heap)
+            if cost > lower_cost.get(zone, (float('inf'), None))[0]:
                 continue
-        if z1 == zone:
-            zones.append(z2)
-        elif z2 == zone:
-            zones.append(z1)
-    return zones
+            neighbours = self._adjacent_zones(self, zone, exclude)
+            for neighbor in neighbours:
+                new_cost = cost + self.network.lookup_hubs[neighbor].cost
+                new_path = path[:]
+                new_path.append(neighbor)
+                low_cost = lower_cost.get(neighbor, (float('inf'), None))[0]
+                if new_cost < low_cost:
+                    lower_cost[neighbor] = new_cost, new_path
+                    heappush(heap, (new_cost, neighbor, new_path))
+                # Condition to give priority to PRIORITY TypeZone
+                elif (
+                    new_cost == low_cost and
+                    get_hub_object[neighbor].zone.name == ZoneType.PRIORITY.name
+                ):
+                    lower_cost[neighbor] = new_cost, new_path
+                    heappush(heap, (new_cost, neighbor, new_path))
+        return lower_cost
+
 
 
 def min_cost(
